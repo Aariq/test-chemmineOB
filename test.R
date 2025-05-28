da08b2590a6c5b0d364b00ff252314df86a6bc10
Eric Scott <scottericr@gmail.com>
2025-05-28 14:06:04 -0700

    test

--- test.R
+++ test.R
@@ -1,3 +1,5 @@
+cat("loading ChemmineOB")
 library(ChemmineOB) #should trigger onload which triggers .supportedPlatforms()
 
+cat("trying ensureOB")
 stopifnot(suppressWarnings(require('ChemmineOB', quietly=T)) && ChemmineOB:::.supportedPlatform())
